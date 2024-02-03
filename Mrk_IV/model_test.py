33c5d7736cb4851cfff8760dc8fd26c1560ccf12
Monte DuMond <gdumond42@gmail.com>
2024-02-02 23:12:56 -0500

    Mrk-IV - Sckit-Learn RandomForestClassifier(Working)

--- Mrk_IV/model_test.py
+++ Mrk_IV/model_test.py
@@ -21,59 +21,59 @@ y = df.iloc[:,-1].values
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
 
-# RUN UN SCALED FOR BETTER RESULTS
-# sc = StandardScaler()
-# X_train = sc.fit_transform(X_train)
-# X_test = sc.transform(X_test)
+# RUN UN SCALED FOR BETTER RESULTS in forests
+sc = StandardScaler()
+X_train_std = sc.fit_transform(X_train)
+X_test_std = sc.transform(X_test)
 
-print("Logistic Regression")
-for e in range(1,101):
-    lr_ovr = LogisticRegression(C=float(e), solver='lbfgs', multi_class='ovr')
-    lr_ovr.fit(X_train, y_train)
-    acc = round(lr_ovr.score(X_test, y_test)*100, 2)
-    print(f'"ovr" - ACC: {acc}%\tc: {float(e)}')
-    if acc > best_acc:
-        best_model = lr_ovr
-        best_acc = acc
-        model_type = 'lr_ovr'
-        print(f'Best Set LR OVR {acc}%')
+# print("Logistic Regression")
+# for e in range(1,101):
+#     lr_ovr = LogisticRegression(C=float(e), solver='lbfgs', multi_class='ovr')
+#     lr_ovr.fit(X_train_std, y_train)
+#     acc = round(lr_ovr.score(X_test_std, y_test)*100, 2)
+#     print(f'"ovr" - ACC: {acc}%\tc: {float(e)}')
+#     if acc > best_acc:
+#         best_model = lr_ovr
+#         best_acc = acc
+#         model_type = 'lr_ovr'
+#         print(f'Best Set LR OVR {acc}%')
 
-    lr_multin = LogisticRegression(C=float(e), solver='lbfgs', multi_class='multinomial', max_iter=10000)
-    lr_multin.fit(X_train, y_train)
-    acc = round(lr_multin.score(X_test, y_test)*100, 2)
-    print(f'"multinomial" ACC: {acc}%\tc: {float(e)}')
-    if acc > best_acc:
-        best_model = lr_multin
-        best_acc = acc
-        model_type = 'lr_multin'
-        print(f'Best Set LR multiN {acc}%')
+#     lr_multin = LogisticRegression(C=float(e), solver='lbfgs', multi_class='multinomial', max_iter=10000)
+#     lr_multin.fit(X_train_std, y_train)
+#     acc = round(lr_multin.score(X_test_std, y_test)*100, 2)
+#     print(f'"multinomial" ACC: {acc}%\tc: {float(e)}')
+#     if acc > best_acc:
+#         best_model = lr_multin
+#         best_acc = acc
+#         model_type = 'lr_multin'
+#         print(f'Best Set LR multiN {acc}%')
 
-print('\n\nDecision Tree')
-for e in range(1,51):
-    tree_model = DecisionTreeClassifier(criterion='entropy', max_depth=e)
-    tree_model.fit(X_train, y_train)
-    acc = round(tree_model.score(X_test, y_test)*100, 2)
-    print(f'tree - ACC: {acc}%\tbranches: {float(e)}')
-    if acc > best_acc:
-        best_model = tree_model
-        best_acc = acc
-        model_type = 'tree'
-        print(f'Best Set tree {acc}%')
+# print('\n\nDecision Tree')
+# for e in range(1,51):
+#     tree_model = DecisionTreeClassifier(criterion='entropy', max_depth=e)
+#     tree_model.fit(X_train, y_train)
+#     acc = round(tree_model.score(X_test, y_test)*100, 2)
+#     print(f'tree - ACC: {acc}%\tbranches: {float(e)}')
+#     if acc > best_acc:
+#         best_model = tree_model
+#         best_acc = acc
+#         model_type = 'tree'
+#         print(f'Best Set tree {acc}%')
 
-print('\n\nForest')
-for e in range(10, 101):
-    forest = RandomForestClassifier(n_estimators=e, n_jobs=2)
-    forest.fit(X_train, y_train)
-    acc = round(forest.score(X_test, y_test)*100, 2)
-    print(f'Forest - ACC: {acc}%\ttrees: {float(e)}')
-    if acc > best_acc:
-        best_model = forest
-        best_acc = acc
-        model_type = 'forest'
-        print(f'Best Set forest {acc}%')
+# print('\n\nForest')
+# for e in range(10, 101):
+#     forest = RandomForestClassifier(n_estimators=e, n_jobs=2)
+#     forest.fit(X_train, y_train)
+#     acc = round(forest.score(X_test, y_test)*100, 2)
+#     print(f'Forest - ACC: {acc}%\ttrees: {float(e)}')
+#     if acc > best_acc:
+#         best_model = forest
+#         best_acc = acc
+#         model_type = 'forest'
+#         print(f'Best Set forest {acc}%')
 
 print('\n\nKNN')
-for i in range(5,101):
+for e in range(5,101):
     knn = KNeighborsClassifier(n_neighbors=e, p=1, metric='minkowski')
     knn.fit(X_train, y_train)  
     acc = round(knn.score(X_test, y_test)*100, 2)
